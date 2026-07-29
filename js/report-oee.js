2939ec6a7f355eb95cd0b2951622c30bec767f89
Delprado <Jonathan.Delprado@analog.com>
2026-07-29 10:21:52 +0800

    oee reports bugfixes and ui adjustments

--- js/report-oee.js
+++ js/report-oee.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+    var direction_arr = [
+        [1, "asc"],
+        [4, "asc"]
+    ];
+
     function exportButton(type) {
         let cols = [];
         let title_object = {};
@@ -20,9 +25,10 @@ $(document).ready(function(){
 
     var oee_report_table = $("#table-oee-report").DataTable({
         // ordering: false,
+        autoWidth: false, //fix tester column null still sorted
         order: [
             [1, 'asc'],
-            [2, 'asc'],
+            [4, 'asc'],
         ],
         responsive: true,
         pageLength: 25,
@@ -47,6 +53,7 @@ $(document).ready(function(){
                 orderable: false,
                 data: null,
                 defaultContent: '',
+                width: "60px",
                 createdCell: function (td, cellData, rowData, row, col) {
                     if (col == 0) {
                         $(td).attr("history-log", rowData[8]);
@@ -54,18 +61,38 @@ $(document).ready(function(){
                 }
             },
             {
-                targets: '_all',   // Column to keep sorted
+                targets: [3, 4, 5, 6],
+                render: function (data, type, row, meta) {
+                    let result = direction_arr.find(row => row.includes(meta.col));
+                    let cast_val = "";
+                    if (result) {
+                        if (type === 'sort') {
+                            if (meta.col == result[0]) {
+                                console.log(meta.col);
+                                if (data === '' || data === null || data === 'null') {
+                                    return result[1] === 'asc'
+                                        ? '\uffff'
+                                        : '';
+                                }
+                            }
+                        }
+                    }
+                    return data;
+                }
+            },
+            {
+                targets: '_all',
                 orderable: false
             }
         ],
     });
 
-    $('button.nav-link[data-bs-toggle="tab"]').on('show.bs.tab', function (e) {
-        let tab = $(this).attr("tab-name");
-        const url = new URL(window.location.href);
-        url.searchParams.set("tab", tab);
-        window.history.pushState('state', 'title', url.href);
-    });
+    // $('button.nav-link[data-bs-toggle="tab"]').on('show.bs.tab', function (e) {
+    //     let tab = $(this).attr("tab-name");
+    //     const url = new URL(window.location.href);
+    //     url.searchParams.set("tab", tab);
+    //     window.history.pushState('state', 'title', url.href);
+    // });
 
     renderOEERows(oee_report_table);
 
@@ -84,11 +111,16 @@ $(document).ready(function(){
     });
 
     $(".dropdown-sort").on("change", function(){
+        
         let first_col = $("#oee-first-column").val();
         let first_dir = $("#oee-first-direction").val();
         let second_col = $("#oee-second-column").val();
         let second_dir = $("#oee-second-direction").val();
-
+        direction_arr = [
+            [parseInt(first_col), first_dir],
+            [parseInt(second_col), second_dir]
+        ];
+        oee_report_table.rows().invalidate().draw();
         oee_report_table.order([
             [parseInt(first_col), first_dir],
             [parseInt(second_col), second_dir]
@@ -103,6 +135,7 @@ $(document).ready(function(){
         });
     });
 
+    //search fields - filters
     $('#table-oee-report thead tr:eq(1) th').each(function (i) {
         $('input', this).on('keyup change', function () {
             if (oee_report_table.column(i).search() !== this.value) {
@@ -116,6 +149,16 @@ $(document).ready(function(){
         });
     });
 
+    //clear button - input type search
+    $('#table-oee-report thead').on('search', 'input[type="search"]', function () {
+        let columnIndex = $(this).closest('th').index();
+
+        oee_report_table
+            .column(columnIndex)
+            .search(this.value)
+            .draw();
+    });
+
     oee_report_table.column(8).search($(".change-user-filter").val()).draw();
 
 });
@@ -123,18 +166,18 @@ $(document).ready(function(){
 function renderOEERows(table){
     let row_arr = [];
     $.each(oee_data['DATA'], function(index, item){
-        let history_level = item['DETAILS'].split("|");
-        let created_by = history_level[0].split(">")[2];
+        let history_logs = item['LOGS'];
+        let created_by = history_logs[history_logs.length - 1].split(">")[2];
         if (item["SITE_NUM"] != "" && item["RES_AREA"]) {
             row_arr.push([
-                JSON.stringify(history_level),
+                JSON.stringify(history_logs),
                 item["SITE_NUM"],
                 item["RES_AREA"],
                 item["TESTER"],
                 item["HANDLER"],
                 item["TEMP_CLASS"],
                 item["MFG_PART_NUM"],
-                item["OEE_VAL"],
+                parseFloat(item["OEE_VAL"]).toFixed(2),
                 created_by
             ]);
         }
@@ -146,15 +189,14 @@ function renderOEERows(table){
 function format(data) {
 
     let history_data = JSON.parse(data[0]);
-    let tree_log_item = "";
-    console.log(history_data);
-    
+    let tree_log_item = ""; 
+
     $.each(history_data, function(index, item){
         let split = item.split(">");
         let convert_date = new Date(split[1].replace(' ', 'T'));
         let badge_color = (split[3] == "NEW_OVERRIDE") ? "success" : "info";
 
-        let oee = split[0];
+        let oee = parseFloat(split[0]).toFixed(2);
         let date = convert_date.toLocaleString();
         let user = split[2];
         let change = split[3];
@@ -172,6 +214,47 @@ function format(data) {
                             '</div>'+
                         '</div>';
     });
+    
+
+    // var first_log = parseFloat(history_data[0].split(">")[0]).toFixed(2);
+    // var has_diff = false;
+
+    // $.each(history_data, function(index, item) {
+    //     var value = parseFloat(item.split(">")[0]).toFixed(2);
+
+    //     if (value !== first_log) {
+    //         has_diff = true;
+    //         return false;
+    //     }
+    // });
+
+    // $.each(history_data, function(index, item){
+    //     let split = item.split(">");
+    //     let convert_date = new Date(split[1].replace(' ', 'T'));
+    //     let badge_color = (split[3] == "NEW_OVERRIDE") ? "success" : "info";
+
+    //     let oee = parseFloat(split[0]).toFixed(2);
+    //     let date = convert_date.toLocaleString();
+    //     let user = split[2];
+    //     let change = split[3];
+
+    //     tree_log_item += '<div class="tree-item">'+
+    //                         '<div style="width: 40%;" class="card">'+
+    //                             '<div class="card-body d-flex justify-content-between">'+
+    //                                 '<span><small>OEE</small><br><h4>'+oee+'</h4></span>'+
+    //                                 '<span><small>User:</small><br><b>'+user+'</b></span>'+
+    //                             '</div>'+
+    //                             '<div class="card-footer d-flex justify-content-between">'+
+    //                                 '<small class="text-body-secondary">Date: '+date+'</small>'+
+    //                                 '<span class="badge text-bg-'+badge_color+'">'+change+'</span>'+
+    //                             '</div>'+
+    //                         '</div>'+
+    //                     '</div>';
+
+    //     if (has_diff === false) {
+    //         return false;
+    //     }
+    // });
 
     return (
         '<div class="tree-timeline pt-4">'+tree_log_item+'</div>'
